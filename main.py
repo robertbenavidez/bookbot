808b8617cf42c6a84f3f35cd610c48e0e6ea8982
Robert Benavidez <robertbenavidez@roberts-mbp.myfiosgateway.com>
2024-02-14 14:22:13 -0500

    letter count

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     print(text)
     words = word_count(text)
     print("word count is:", words)
+    letter_count = get_chars(text)
+    print('letter count', letter_count)
 
 def word_count(book):
     words = book.split()
@@ -11,6 +13,17 @@ def word_count(book):
     for word in words:
         count += 1
     return count
+
+def get_chars(book):
+    chars = {}
+
+    for word in book:
+        lowered = word.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 def get_book_txt(path):
     with open(path) as f:
