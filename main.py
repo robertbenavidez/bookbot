c394fb537cf1aad0a556baf6d8d6f7290ecee8fe
Robert Benavidez <robertbenavidez@roberts-mbp.myfiosgateway.com>
2024-02-14 14:14:29 -0500

    added word count functionality

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     path_name = "books/frankenstein.txt"
     text = get_book_txt(path_name)
     print(text)
+    words = word_count(text)
+    print("word count is:", words)
+
+def word_count(book):
+    words = book.split()
+    count = 0
+    for word in words:
+        count += 1
+    return count
 
 def get_book_txt(path):
     with open(path) as f:
