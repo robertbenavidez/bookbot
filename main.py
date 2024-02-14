44076d2f7dbb2781bbaa8eec21362168a4c5e2f6
Robert Benavidez <robertbenavidez@roberts-mbp.myfiosgateway.com>
2024-02-14 14:42:03 -0500

    project completed

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 def main():
     path_name = "books/frankenstein.txt"
     text = get_book_txt(path_name)
-    print(text)
     words = word_count(text)
-    print("word count is:", words)
     letter_count = get_chars(text)
-    print('letter count', letter_count)
+    chars_sorted_list = chars_dict_to_sorted_list(letter_count)
+
+    print(f"--- Begin report of {path_name} ---")
+    print(f"{words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 def word_count(book):
     words = book.split()
@@ -13,6 +22,18 @@ def word_count(book):
     for word in words:
         count += 1
     return count
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def get_chars(book):
     chars = {}
@@ -29,5 +50,4 @@ def get_book_txt(path):
     with open(path) as f:
         return f.read()
 
-if __name__ == '__main__':
-    main()
+main()
